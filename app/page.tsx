3e03d415590494aa21ea9dd9f05bcb1db790e8ac
konoyubi-web01 <xcnsn723@yahoo.co.jp>
2025-11-18 12:35:15 +0900

    タイトルとディスクリプションが表示されない問題6 Home window

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
+'use client';
+
 import Home from "./Home";
-
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-// import Home from './HomeClient'; // ← こちらは 'use client' のファイル
 
 export default function Page() {
   return <Home />;
